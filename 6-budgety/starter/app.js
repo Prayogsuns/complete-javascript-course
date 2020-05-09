934df1939138c84b3fc2fcb5802c4dfe36935edf
Prayogsuns <prayogsuns@gmail.com>
2020-05-10 00:50:22 +0530

    6-budgety Step-implement-module-pattern mv0.04

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,9 +1,54 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
 
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            //Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+
+        }
+    };
 
 })();
+
 
 // UI CONTROLLER
 var UIController = (function () {
@@ -17,29 +62,44 @@ var UIController = (function () {
     };
 
     return {
-        domStrings: DOMStrings,
         getinput: function () {
             return {
-                type: document.querySelector(this.domStrings.inputType).value, // will be either inc or exp
-                description: document.querySelector(this.domStrings.inputDescription).value,
-                value: document.querySelector(this.domStrings.inputValue).value
+                type: document.querySelector(DOMStrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
             };
+        },
+        getDOMStrings: function () {
+            return DOMStrings;
         }
     };
 
 })();
 
+
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.domStrings;
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
     var ctrlAddItem = function () {
+        var input, newItem;
 
         // 1. Get the filled input data
-        var input = UICtrl.getinput();
-        console.log(input);
+        input = UICtrl.getinput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
@@ -47,23 +107,15 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         // 5. Display the budget in the UI
 
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-
-        // console.log(event);
-
-        if (event.keyCode === 13 || event.which === 13) {
-
-            // console.log('ENTER is pressed');
-
-            ctrlAddItem();
-
+    return {
+        init: function () {
+            console.log("Application has Started.");
+            setupEventListeners();
         }
-
-    });
+    }
 
 })(budgetController, UIController);
 
+controller.init();
